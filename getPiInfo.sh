45648b9c4a6beba999bbef5e107814cf72fe909e
byazit <byazit@gmail.com>
2020-12-06 15:40:09 +0100

    get raspberry pi information

---
+++ getPiInfo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "--------------------OS info--------------------------"
+uname -a
+echo "--------------------CPU info-------------------------"
+cat /proc/cpuinfo
+echo "--------------------Memory info----------------------"
+cat /proc/meminfo
+echo "--------------------Size&partiton--------------------"
+cat /proc/partitions
+echo "--------------------Pi version-----------------------"
+cat /proc/version
+echo "--------------------Tempature------------------------"
+vcgencmd measure_temp
+echo "--------------------Memory split CPU&GPU-------------"
+vcgencmd get_mem arm && vcgencmd get_mem gpu
+echo "-------------------System memory---------------------"
+free -m -h
